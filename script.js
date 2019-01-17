bac5c6183d980a994c9cb6005fae13c20214e53c
Th30s <43751800+Th30s@users.noreply.github.com>
2019-01-16 22:30:13 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#bgone").click(function(){
+    $("#betusdeletus").toggle();
+});
+
+
+
+//Spell #2
+
+$("mostus snapus").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
